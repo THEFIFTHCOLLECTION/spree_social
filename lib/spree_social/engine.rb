5e6d6e34b2b98dd42acf66f9a1506d9beb5bf6b9
truongduyng <truongduyng@gmail.com>
2015-09-16 06:51:08 +0700

    fix for fb api 2.4, get email of user

--- lib/spree_social/engine.rb
+++ lib/spree_social/engine.rb
@@ -51,13 +51,15 @@ module OmniAuth
     class Facebook < OAuth2
 
       MOBILE_USER_AGENTS =  'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
-                              'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
-                              'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
-                              'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
-                              'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|' +
-                              'mobile'
+      'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
+      'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
+      'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
+      'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|' +
+      'mobile'
+
       def request_phase
         options[:scope] ||= "email"
+        options[:info_fields] ||= "email"
         options[:display] = mobile_request? ? 'touch' : 'page'
         super
       end
