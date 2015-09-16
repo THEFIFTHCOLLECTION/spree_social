733f48e3d28b8348f39f14ad86267cce44ee5887
truongduyng <truongduyng@gmail.com>
2015-09-16 07:08:17 +0700

    fix associate user with current order

--- app/controllers/spree/omniauth_callbacks_controller.rb
+++ app/controllers/spree/omniauth_callbacks_controller.rb
@@ -8,44 +8,41 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def self.provides_callback_for(*providers)
     providers.each do |provider|
       class_eval <<-FUNCTION_DEFS, __FILE__, __LINE__ + 1
-        ssl_allowed :#{provider}
-        def #{provider}
-          if request.env["omniauth.error"].present?
-            flash[:error] = I18n.t("devise.omniauth_callbacks.failure", :kind => auth_hash['provider'], :reason => Spree.t(:user_was_not_valid))
-            redirect_back_or_default(root_url)
+      ssl_allowed :#{provider}
+      def #{provider}
+        if request.env["omniauth.error"].present?
+          flash[:error] = I18n.t("devise.omniauth_callbacks.failure", :kind => auth_hash['provider'], :reason => Spree.t(:user_was_not_valid))
+          redirect_back_or_default(root_url)
+          return
+        end
+
+        authentication = Spree::UserAuthentication.find_by_provider_and_uid(auth_hash['provider'], auth_hash['uid'])
+
+        if authentication.present?
+          flash[:notice] = t("devise.omniauth_callbacks.success", :kind => auth_hash['provider'])
+          associate_order_with_user(authentication)
+          sign_in_and_redirect :spree_user, authentication.user
+        elsif spree_current_user
+          spree_current_user.apply_omniauth(auth_hash)
+          spree_current_user.save!
+          associate_order_with_user(authentication)
+          flash[:notice] = t("devise.sessions.signed_in")
+          redirect_back_or_default(account_url)
+        else
+          user = Spree::User.find_by_email(auth_hash['info']['email']) || Spree::User.new
+          user.apply_omniauth(auth_hash)
+          if user.save
+            flash[:notice] = I18n.t("devise.omniauth_callbacks.success", :kind => auth_hash['provider'])
+            associate_order_with_user(authentication)
+            sign_in_and_redirect :spree_user, user
+          else
+            session[:omniauth] = auth_hash.except('extra')
+            flash[:notice] = Spree.t(:one_more_step, :kind => auth_hash['provider'].capitalize)
+            redirect_to new_spree_user_registration_url
             return
           end
-
-          authentication = Spree::UserAuthentication.find_by_provider_and_uid(auth_hash['provider'], auth_hash['uid'])
-
-          if authentication.present?
-            flash[:notice] = t("devise.omniauth_callbacks.success", :kind => auth_hash['provider'])
-            sign_in_and_redirect :spree_user, authentication.user
-          elsif spree_current_user
-            spree_current_user.apply_omniauth(auth_hash)
-            spree_current_user.save!
-            flash[:notice] = t("devise.sessions.signed_in")
-            redirect_back_or_default(account_url)
-          else
-            user = Spree::User.find_by_email(auth_hash['info']['email']) || Spree::User.new
-            user.apply_omniauth(auth_hash)
-            if user.save
-              flash[:notice] = I18n.t("devise.omniauth_callbacks.success", :kind => auth_hash['provider'])
-              sign_in_and_redirect :spree_user, user
-            else
-              session[:omniauth] = auth_hash.except('extra')
-              flash[:notice] = Spree.t(:one_more_step, :kind => auth_hash['provider'].capitalize)
-              redirect_to new_spree_user_registration_url
-              return
-            end
-          end
-
-          if current_order
-            user = spree_current_user || authentication.user
-            current_order.associate_user!(user)
-            session[:guest_token] = nil
-          end
         end
+      end
       FUNCTION_DEFS
     end
   end
@@ -65,5 +62,13 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def auth_hash
     request.env["omniauth.auth"]
+  end
+
+  def associate_order_with_user(authentication)
+    if current_order
+      user = spree_current_user || authentication.user
+      current_order.associate_user!(user)
+      session[:guest_token] = nil
+    end
   end
 end
